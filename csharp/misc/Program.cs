23b9eb92a8860e64c937dc1269c322276d740d80
Erica <ericaesnider@gmail.com>
2017-09-08 20:29:16 -0500

    add csharp misc algorithms

---
+++ csharp/misc/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace misc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(IsPrimeRange(22));
+        }
+
+        static int? IsPrime(int n){
+            for (int i = 2; i <= Math.Floor((double) n/2); i++)
+            {
+                if (n%i == 0)
+                {
+                    return null;
+                }
+            }
+            return n;
+        }
+
+        static List<int> IsPrimeRange(int x)
+        {
+            List<int> arr = new List<int>();
+            for (int n = 2; n <= x; n++)
+            {
+                if (IsPrime(n) != null) 
+                {
+                    arr.Add(n);
+                }
+            }
+            return arr;
+        }
+    }
+}
